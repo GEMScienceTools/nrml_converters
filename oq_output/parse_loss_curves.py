442e9fad6a486feeb9c94849b44f25fa2b1a7217
Vitor Silva <vitor.silva@eucentre.it>
2014-02-18 22:11:38 +0100

    even smaller changes

--- oq_output/parse_loss_curves.py
+++ oq_output/parse_loss_curves.py
@@ -5,7 +5,7 @@
 #
 # Copyright (c) 2010-2014, GEM Foundation, V. Silva
 #
-# The Risk Modeller's Toolkit is free software: you can redistribute
+# The Risk Modellers Toolkit is free software: you can redistribute
 # it and/or modify it under the terms of the GNU Affero General Public
 # License as published by the Free Software Foundation, either version
 # 3 of the License, or (at your option) any later version.
@@ -15,7 +15,7 @@
 #
 # DISCLAIMER
 # 
-# The software Risk Modeller's Toolkit (rmtk) provided herein
+# The software Risk Modellers Toolkit (rmtk) provided herein
 # is released as a prototype implementation on behalf of
 # scientists and engineers working within the GEM Foundation (Global
 # Earthquake Model).
@@ -24,18 +24,18 @@
 # hope that it will be useful to the scientific, engineering, disaster
 # risk and software design communities.
 #
-# The software is NOT distributed as part of GEM’s OpenQuake suite
+# The software is NOT distributed as part of GEMs OpenQuake suite
 # (http://www.globalquakemodel.org/openquake) and must be considered as a
 # separate entity. The software provided herein is designed and implemented
 # by scientific staff. It is not developed to the design standards, nor
 # subject to same level of critical review by professional software
-# developers, as GEM’s OpenQuake software suite.
+# developers, as GEMs OpenQuake software suite.
 #
 # Feedback and contribution to the software is welcome, and can be
 # directed to the risk scientific staff of the GEM Model Facility
 # (risk@globalquakemodel.org).
 #
-# The Risk Modeller's Toolkit (rmtk) is therefore distributed WITHOUT
+# The Risk Modellers Toolkit (rmtk) is therefore distributed WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
 # for more details.
